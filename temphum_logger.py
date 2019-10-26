720cd8fe6bae434a0855724dd47829f5daaac3ca
Philip Basford <pjb@ecs.soton.ac.uk>
2019-10-26 15:01:07 +0100

    First version untested

---
+++ temphum_logger.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+"""
+    Philip Basford
+    October 2019
+"""
+
+import time
+from argparse import ArgumentParser
+import MySQLdb
+import Adafruit_DHT
+from w1thermsensor import W1ThermSensor
+from temphum_database import TempHumDatabase
+
+
+def read_w1():
+    """
+        Read the one-wire sensor to get a temperature reading
+    """
+    sensor = W1ThermSensor()
+    return sensor.get_temperature()
+
+def read_am2302(pin):
+    """
+        Read temperature and humidity data from an AM2302 sensor
+    """
+    humidity, temperature = Adafruit_DHT.read(Adafruit_DHT.DHT22, pin)
+    return (temperature, humidity)
+
+
+def loop(device, config_file, sensor_type, interval, pin=None):
+    """
+        Loop indefintely and take the readings from the specified sensor
+    """
+    while True:
+        db = TempHumDatabase(config_file)
+        try:
+            while True:
+                temperature = None
+                humidity = None
+                if sensor_type == "AM2302":
+                    if pin is None:
+                        raise ValueError("No pin specified")
+                    else:
+                        temperature, humidity = read_am2302(pin)
+                elif sensor_type == "w1":
+                    temperature = read_w1()
+                else:
+                    raise ValueError("Unknown sensor type")
+                db.store_reading(device, temperature, humidity)
+                time.sleep(interval)
+        except MySQLdb.OperationError as exp:
+            print(exp)
+            time.sleep(120)
+
+if __name__ == "__main__":
+    PARSER = ArgumentParser(
+        description="Read temperature (and humidity) data and submit to database")
+    PARSER.add_argument(
+        "-c",
+        "--config",
+        help="Configuration file to use for database access",
+        required=True,
+        action="store")
+    PARSER.add_argument(
+        "-n",
+        "--name",
+        required=True,
+        action="store",
+        help="Location name")
+    SENSOR_OPTION = PARSER.add_mutually_exclusive_group(required=True)
+    SENSOR_OPTION.add_argument(
+        "-a",
+        "--am2302",
+        action="store_true",
+        help="Read from an AM2302")
+    SENSOR_OPTION.add_argument(
+        "-w",
+        "--w1",
+        action="store_true",
+        help="Read a 1-wire sensor")
+    PARSER.add_argument(
+        "-i",
+        "--interval",
+        action="store",
+        required=True,
+        help="The interval between readings")
+    PARSER.add_argument(
+        "-p",
+        "--pin",
+        action="store",
+        type=int,
+        help="The pin to use for the sensor")
+    ARGS = PARSER.parse_args()
+    if ARGS.am2302 and ARGS.pin is None:
+        PARSER.error("AM2302 requires a pin to be set")
+    SENSOR = ""
+    if ARGS.am2302:
+        SENSOR = "am2302"
+    elif ARGS.w1:
+        SENSOR="w1"
+    else:
+        print("Unknown sensor type")
+        exit(2)
+    if ARGS.pin is None:
+        PIN = None
+    else:
+        PIN = ARGS.pin
+    loop(ARGS.name, ARGS.config, SENSOR, ARGS.interval, PIN)
