3aae0f191e2fbff2e5927c85aa6373051218e24b
Philip Basford <pjb@ecs.soton.ac.uk>
2019-10-26 16:26:37 +0100

    Now use pip to install module rather than compiling it

--- setup.sh
+++ setup.sh
@@ -3,13 +3,5 @@
 sudo raspi-config nonint do_memory_split 16
 sudo apt install python3-pip python3-setuptools
 
-echo "---------- Install package for AM2302 ----------"
-cd $HOME
-if [ -d "Adafruit_Python_DHT" ]; then
-  sudo rm -rf Adafruit_Python_DHT
-fi
-git clone https://github.com/adafruit/Adafruit_Python_DHT.git
-cd Adafruit_Python_DHT
-sudo python3 setup.py install
-
+sudo pip3 install Adafruit_DHT
 pip3 install w1thermsensor
