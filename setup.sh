5e4e25405625a2620b0b2b7b2dc31358c21ebe08
Philip Basford <pjb@ecs.soton.ac.uk>
2019-10-26 16:28:36 +0100

    More setup configuration

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo raspi-config nonint do_memory_split 16
+sudo raspi-config nonint do_w1 0
 sudo apt install python3-pip python3-setuptools
 
 echo "---------- Install package for AM2302 ----------"
@@ -13,3 +14,4 @@ cd Adafruit_Python_DHT
 sudo python3 setup.py install
 
 pip3 install w1thermsensor
+pip install mysqlclient
