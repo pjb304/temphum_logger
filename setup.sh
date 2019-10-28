a86755271f29187528e9ff26c3d3d7a5ac1efff9
Philip Basford <pjb@ecs.soton.ac.uk>
2019-10-28 21:10:39 +0000

    setup updates

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo raspi-config nonint do_memory_split 16
 sudo raspi-config nonint do_w1 0
-sudo apt install python3-pip python3-setuptools
+sudo apt install python3-pip python3-setuptools  libmariadb-dev
 
-pip install mysqlclient configobj w1thermsensor Adafruit_DHT
+pip3 install mysqlclient configobj w1thermsensor Adafruit_DHT
 
